a31fe8d9dd4999db7c4e9a0a39e7d23752a94dc7
sassi walid <sassi.walid@outlook.com>
2018-02-11 17:27:31 +0100

    some changes

---
+++ FeminBio/FeminBio-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  FeminBio-Bridging-Header.h
+//  FeminBio
+//
+//  Created by Walid Sassi on 11/02/2018.
+//  Copyright © 2018 Walid Sassi. All rights reserved.
+//
+
+#ifndef FeminBio_Bridging_Header_h
+#define FeminBio_Bridging_Header_h
+
+
+#endif /* FeminBio_Bridging_Header_h */
